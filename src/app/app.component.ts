663fa1c72c6a0d88e6b80ec208c0ed0bbea6b99d
rch850 <rich850@gmail.com>
2018-03-25 23:04:38 +0900

    👍 throttle keydown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import * as screenfull from 'screenfull';
 import * as toastr from 'toastr';
 import { KeyboardService } from './keyboard.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/throttleTime';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-root',
@@ -16,14 +21,16 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     toastr.info('画面をクリックすると全画面になります');
-    document.addEventListener('keydown', (ev) => {
-      this.kana = this.keyboard.convert(ev);
-      if (this.kana !== '') {
-        speechSynthesis.cancel();
-        const utter = new SpeechSynthesisUtterance(this.kana);
-        utter.lang = 'ja';
-        speechSynthesis.speak(utter);
-      }
+    Observable.fromEvent(document, 'keydown')
+        .map((ev: KeyboardEvent) => this.keyboard.convert(ev))
+        .filter(kana => kana !== '')
+        .throttleTime(500)
+        .subscribe(kana => {
+      this.kana = kana;
+      speechSynthesis.cancel();
+      const utter = new SpeechSynthesisUtterance(this.kana);
+      utter.lang = 'ja';
+      speechSynthesis.speak(utter);
     });
   }
 
