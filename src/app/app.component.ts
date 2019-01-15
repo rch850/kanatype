1f862d1a61a0bbaf4ed8a2fc9d862cc5e625f6fb
rch850 <rich850@gmail.com>
2019-01-16 01:46:10 +0900

    🆙 bump deps
    - ng update
    - screenfull needs to update to continue work.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import * as screenfull from 'screenfull';
 import * as toastr from 'toastr';
 import { KeyboardService } from './keyboard.service';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/operator/throttleTime';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/filter';
+import { fromEvent } from 'rxjs';
+import { map, filter, throttleTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -21,11 +18,13 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     toastr.info('画面をクリックすると全画面になります');
-    Observable.fromEvent(document, 'keydown')
-        .map((ev: KeyboardEvent) => this.keyboard.convert(ev))
-        .filter(kana => kana !== '')
-        .throttleTime(500)
-        .subscribe(kana => {
+    fromEvent(document, 'keydown')
+    .pipe(
+      map((ev: KeyboardEvent) => this.keyboard.convert(ev)),
+      filter(kana => kana !== ''),
+      throttleTime(500)
+    )
+    .subscribe(kana => {
       this.kana = kana;
       speechSynthesis.cancel();
       const utter = new SpeechSynthesisUtterance(this.kana);
